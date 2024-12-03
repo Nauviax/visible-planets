a2d4c3f666005858deff08bb750659de488fd8f9
Nauviax <nauviax@icloud.com>
2024-12-04 09:12:28 +1300

    Nuclear option for fixing invalid renders. Should prevent all related future crashes.

--- control_stuff/render_planets.lua
+++ control_stuff/render_planets.lua
@@ -125,14 +125,14 @@ end)
 
 -- Parallax and rotation animation function, given a render.
 local function fancy_animations(render)
-	if render.player and PARALLAX_ENABLED then -- Will end up correct only for last player watching a given planet.
+	if render.player and PARALLAX_ENABLED then -- Will end up correct only for one player watching a given planet.
 		local player_position = render.player.position
 		local planet_position = render.obj.target.position
 		local x_offset = (player_position.x - planet_position.x) / PARALLAX_FACTOR
 		local y_offset = (player_position.y - planet_position.y) / PARALLAX_FACTOR
 		render.obj.oriented_offset = {x_offset, y_offset} -- Will not affect literal position.
 	end
-	if ROTATION_ENABLED then -- Will double rotate if two players watching same planet.
+	if ROTATION_ENABLED then
 		local new_ang = render.obj.orientation + ROTATION_SPEED
 		if new_ang >= 1 then
 			new_ang = new_ang - 1
@@ -148,23 +148,27 @@ end
 script.on_event(defines.events.on_tick, function(event)
 	-- Arrival animation
 	for index, render in pairs(storage.visible_planets_renders_grow) do
-		if render.animation_progress < 1.0 then
-			local eased = render.animation_progress * (2 - render.animation_progress) -- Fast at low dur, slow at high dur.
-			local scale = PLANET_SCALE * eased
-			render.obj.x_scale = scale
-			render.obj.y_scale = scale
-			render.obj.target = {x = PLANET_POS_X, y = PLANET_POS_Y - (1 - eased) * PLANET_ARRIVE_DIST}
-			fancy_animations(render) -- Animate fancy
-			render.animation_progress = render.animation_progress + planet_progress_per_tick
-		else
-			storage.visible_planets_renders_still[index] = render
+		if not render.obj.valid then
 			storage.visible_planets_renders_grow[index] = nil
+		else
+			if render.animation_progress < 1.0 then
+				local eased = render.animation_progress * (2 - render.animation_progress) -- Fast at low dur, slow at high dur.
+				local scale = PLANET_SCALE * eased
+				render.obj.x_scale = scale
+				render.obj.y_scale = scale
+				render.obj.target = {x = PLANET_POS_X, y = PLANET_POS_Y - (1 - eased) * PLANET_ARRIVE_DIST}
+				fancy_animations(render) -- Animate fancy
+				render.animation_progress = render.animation_progress + planet_progress_per_tick
+			else
+				storage.visible_planets_renders_still[index] = render
+				storage.visible_planets_renders_grow[index] = nil
+			end
 		end
 	end
 
 	-- Departure animation
 	for index, render in pairs(storage.visible_planets_renders_shrink) do
-		if not render.obj.valid then -- Should solve an issue that someone had. Wasn't able toproduce, but this should be good.
+		if not render.obj.valid then
 			storage.visible_planets_renders_shrink[index] = nil
 		else
 			if render.animation_progress > 0.0 then
@@ -183,26 +187,11 @@ script.on_event(defines.events.on_tick, function(event)
 	end
 
 	-- Standing animation
-	for _, render in pairs(storage.visible_planets_renders_still) do
-		fancy_animations(render)
-	end
-end)
-
--- AFTER a surface is deleted, check if any planet renders have become invalid. If so, remove them.
-script.on_event(defines.events.on_surface_deleted, function(event)
 	for index, render in pairs(storage.visible_planets_renders_still) do
 		if not render.obj.valid then
 			storage.visible_planets_renders_still[index] = nil
-		end
-	end
-	for index, render in pairs(storage.visible_planets_renders_grow) do
-		if not render.obj.valid then
-			storage.visible_planets_renders_grow[index] = nil
-		end
-	end
-	for index, render in pairs(storage.visible_planets_renders_shrink) do
-		if not render.obj.valid then
-			storage.visible_planets_renders_shrink[index] = nil
+		else
+			fancy_animations(render)
 		end
 	end
 end)
