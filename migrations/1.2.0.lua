92932f8bce80efa847daee4ba625fc3e9722ab9a
Nauviax <nauviax@icloud.com>
2024-12-03 18:24:12 +1300

    Fixed a migration issue, bumped version

--- migrations/1.2.0.lua
+++ migrations/1.2.0.lua
@@ -7,10 +7,10 @@ storage.visible_planets_renders_still = stills
 
 -- Remove growing and shrinking planets. Too much effort to corectly convert them, and should regenerate in most cases anyway.
 for _, sprite in pairs(storage.visible_planets_renders_grow) do
-	sprite.obj.destroy()
+	sprite.destroy()
 end
 storage.visible_planets_renders_grow = {}
 for _, sprite in pairs(storage.visible_planets_renders_shrink) do
-	sprite.obj.destroy()
+	sprite.destroy()
 end
 storage.visible_planets_renders_shrink = {}
