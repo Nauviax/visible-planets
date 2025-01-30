783d5e030175820f33ea700a0eccb2f5f0f1b931
Nauviax <nauviax@icloud.com>
2025-01-30 14:02:46 +1300

    Added background renders with PlanetLib thanks to MeteorSwarm/nicholasgower

--- data_stuff/sprite_prototypes.lua
+++ data_stuff/sprite_prototypes.lua
@@ -1,6 +1,11 @@
 -- Constants
 local sprite_goal_size = 512 -- Sprites that have a different size will be scaled to this size.
 local use_blacklist = not settings.startup["visible-planets-override-show-planets"].value -- If true, override and *DON'T* use blacklist. I got it backwards.
+local use_planetslib_compat = settings.startup["visible-planets-planetslib-compat"].value -- If true, add PlanetsLib layers to planets and moons.
+local planetslib_scale = settings.startup["visible-planets-planetslib-scale"].value
+local planetslib_x = settings.startup["visible-planets-planetslib-x"].value
+local planetslib_y = settings.startup["visible-planets-planetslib-y"].value
+local planetslib_tint = settings.startup["visible-planets-planetslib-tint"].value
 
 -- Create SpritePrototype for each planet
 local planet_overrides = vp_get_planet_overrides() -- Defined in separate file
@@ -30,13 +35,64 @@ local function create_planet_sprite_prototype(planet)
     local sprite_prototype = {
         type = "sprite",
         name = name,
+        layers = {}, -- Added below
+    }
+    local main_layer = { -- Large planet render
         filename = icon,
         size = icon_size,
         scale = scale_override * (sprite_goal_size/icon_size), -- Scale down large sprites. Shouldn't reduce resolution.
-        -- mipmap_count = 1,
         flags = { "linear-minification", "linear-magnification" }, -- Prevent pixels showing.
     }
-    data:extend { sprite_prototype }
+
+    -- PlanetLib compat, if it exists and is enabled.
+    if use_planetslib_compat and mods["PlanetsLib"] and planet.surface_properties then
+        -- Get the planet this "moon" is orbiting, if any.
+        local parent_planet_str = planet.surface_properties["parent-planet-str"]
+        -- Add parent layer if moon, or moon layer if parent of a moon.
+        if parent_planet_str then -- Is a moon
+            for _, other_planet in pairs(data.raw["planet"]) do
+                if other_planet.surface_properties and other_planet.surface_properties["planet-str"] == parent_planet_str then
+                    -- Add layer
+                    log("Adding PlanetsLib compat for moon " .. planet.name .. " orbiting " .. other_planet.name)
+                    table.insert(sprite_prototype.layers, {
+                        filename = other_planet.starmap_icon,
+                        size = other_planet.starmap_icon_size,
+                        scale = 0.2 * (sprite_goal_size/other_planet.starmap_icon_size),
+                        flags = { "linear-minification", "linear-magnification" },
+                        tint = { r = 0.5, g = 0.5, b = 0.5, a = 1 }, -- Darken
+                    })
+                end
+            end
+        else -- Is a planet, might have moon(s)
+            local planet_str = planet.surface_properties["planet-str"]
+            for _, other_planet in pairs(data.raw["planet"]) do
+                if other_planet.surface_properties and other_planet.surface_properties["parent-planet-str"] == planet_str then
+                    -- Add layer
+                    log("Adding PlanetsLib compat for " .. planet.name .. " with moon " .. other_planet.name)
+                    table.insert(sprite_prototype.layers, {
+                        filename = other_planet.starmap_icon,
+                        size = other_planet.starmap_icon_size,
+                        scale = planetslib_scale * (sprite_goal_size/other_planet.starmap_icon_size),
+                        flags = { "linear-minification", "linear-magnification" },
+                        tint = { r = planetslib_tint, g = planetslib_tint, b = planetslib_tint, a = 1 }, -- Darken
+                    })
+                end
+            end
+        end
+        -- Arrange bodies around main body.
+        local num_children = #sprite_prototype.layers
+        local shift_x = planetslib_x -- Initial shift, top left corner.
+        local shift_y = planetslib_y
+        for _,child in pairs(sprite_prototype.layers) do -- Rotate background bodies about main body.
+            child.shift = {shift_x, shift_y}
+            shift_x = (shift_x * math.cos(math.pi/num_children)) - (shift_y * math.sin(math.pi/num_children))
+            shift_y = (shift_x * math.sin(math.pi/num_children)) - (shift_y * math.cos(math.pi/num_children))
+        end
+    end
+
+    -- Add main planet as top layer
+    table.insert(sprite_prototype.layers, main_layer)
+    data:extend{sprite_prototype}
 end
 
 -- Create SpritePrototypes for each planet not in the blacklist
--- settings.lua
+++ settings.lua
@@ -99,5 +99,45 @@ data:extend({
         name = "visible-planets-override-show-planets",
         setting_type = "startup",
         default_value = false,
+        order = "a[general]-a"
+    },
+    {
+        type = "bool-setting",
+        name = "visible-planets-planetslib-compat",
+        setting_type = "startup",
+        default_value = true,
+        order = "b[planetslib]-a"
+    },
+    {
+        type = "double-setting",
+        name = "visible-planets-planetslib-scale",
+        setting_type = "startup",
+        default_value = 0.2,
+        minimum_value = 0,
+        maximum_value = 10,
+        order = "b[planetslib]-b"
+    },
+    {
+        type = "double-setting",
+        name = "visible-planets-planetslib-x",
+        setting_type = "startup",
+        default_value = -7.5,
+        order = "b[planetslib]-c"
+    },
+    {
+        type = "double-setting",
+        name = "visible-planets-planetslib-y",
+        setting_type = "startup",
+        default_value = -7.5,
+        order = "b[planetslib]-d"
+    },
+    {
+        type = "double-setting",
+        name = "visible-planets-planetslib-tint",
+        setting_type = "startup",
+        default_value = 0.5,
+        minimum_value = 0,
+        maximum_value = 1,
+        order = "b[planetslib]-e"
     }
 })
