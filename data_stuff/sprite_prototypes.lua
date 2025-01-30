b1786fe8d0ee76918986c87e46741b3d65e7f924
Nauviax <nauviax@icloud.com>
2025-01-30 14:47:17 +1300

    Added safety starmap icon checks, changelog

--- data_stuff/sprite_prototypes.lua
+++ data_stuff/sprite_prototypes.lua
@@ -45,34 +45,39 @@ local function create_planet_sprite_prototype(planet)
     }
 
     if use_planetslib_compat and mods["PlanetsLib"] then
-
         local orbit = planet.orbit
         if orbit and orbit.parent and orbit.parent.type and orbit.parent.type == "planet" and orbit.parent.name then
             local parent = data.raw["planet"][orbit.parent.name]
-            log("Adding PlanetsLib compat for moon " .. planet.name .. " orbiting " .. parent.name)
-            table.insert(sprite_prototype.layers, {
-                filename = parent.starmap_icon,
-                size = parent.starmap_icon_size,
-                scale = 0.2 * (sprite_goal_size/parent.starmap_icon_size),
-                flags = { "linear-minification", "linear-magnification" },
-                tint = { r = 0.5, g = 0.5, b = 0.5, a = 1 }, -- Darken
-            })
-        end
-
-        -- Find all children:
-        for _, child in pairs(data.raw["planet"]) do
-            if child.orbit and child.orbit.parent and child.orbit.parent.type and child.orbit.parent.type == "planet" and child.orbit.parent.name == planet.name then
-                log("Adding PlanetsLib compat for " .. planet.name .. " with moon " .. child.name)
+            if parent.starmap_icon == nil then
+                log("Skipping PlanetsLib compat for moon " .. planet.name .. " orbiting " .. parent.name .. "; Parent starmap icon missing.")
+            else
+                log("Adding PlanetsLib compat for moon " .. planet.name .. " orbiting " .. parent.name)
                 table.insert(sprite_prototype.layers, {
-                    filename = child.starmap_icon,
-                    size = child.starmap_icon_size,
-                    scale = planetslib_scale * (sprite_goal_size/child.starmap_icon_size),
+                    filename = parent.starmap_icon,
+                    size = parent.starmap_icon_size,
+                    scale = 0.2 * (sprite_goal_size/parent.starmap_icon_size),
                     flags = { "linear-minification", "linear-magnification" },
-                    tint = { r = planetslib_tint, g = planetslib_tint, b = planetslib_tint, a = 1 }, -- Darken
+                    tint = { r = 0.5, g = 0.5, b = 0.5, a = 1 }, -- Darken
                 })
             end
         end
-
+        -- Find all children:
+        for _, child in pairs(data.raw["planet"]) do
+            if child.orbit and child.orbit.parent and child.orbit.parent.type and child.orbit.parent.type == "planet" and child.orbit.parent.name == planet.name then
+                if child.starmap_icon == nil then
+                    log("Skipping PlanetsLib compat for " .. planet.name .. " with moon " .. child.name .. "; Moon starmap icon missing.")
+                else
+                    log("Adding PlanetsLib compat for " .. planet.name .. " with moon " .. child.name)
+                    table.insert(sprite_prototype.layers, {
+                        filename = child.starmap_icon,
+                        size = child.starmap_icon_size,
+                        scale = planetslib_scale * (sprite_goal_size/child.starmap_icon_size),
+                        flags = { "linear-minification", "linear-magnification" },
+                        tint = { r = planetslib_tint, g = planetslib_tint, b = planetslib_tint, a = 1 }, -- Darken
+                    })
+                end
+            end
+        end
         -- Arrange bodies around main body.
         local num_children = #sprite_prototype.layers
         local shift_x = planetslib_x -- Initial shift, top left corner.
